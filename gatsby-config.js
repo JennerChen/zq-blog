4429ba19df65ad21cd986a8d8201a20755d6d294
zhangqing <zhangqing332@live.com>
2018-12-21 10:58:17 +0800

    add circleci config

--- gatsby-config.js
+++ gatsby-config.js
@@ -47,7 +47,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-131289526-1`,
       },
     },
     `gatsby-plugin-feed`,
