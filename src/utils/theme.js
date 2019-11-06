ab99f8fbdedbe102669ee23bfe6fdf63af11a784
zhangqing <zhangqing332@live.com>
2019-11-06 09:40:55 +0800

    refactor(css):

--- src/utils/theme.js
+++ src/utils/theme.js
@@ -14,9 +14,6 @@ pre[class*="language-"] {
   word-wrap: normal;
   line-height: 1.5;
   margin-bottom: 0;
-  //max-height: 80em;
-  -moz-tab-size: 4;
-  -o-tab-size: 4;
   tab-size: 4;
 
   -webkit-hyphens: none;
