b632aa6b31610abbdb4e8b7d1c0a802be38e9fd8
zhangqing <zhangqing332@live.com>
2020-03-26 14:01:51 +0800

    chore: 修改 阅读按钮样式

--- src/components/PostNavButton.js
+++ src/components/PostNavButton.js
@@ -3,24 +3,54 @@ import React, { Component } from 'react'
 import styled from 'styled-components'
 import { rhythm } from '../utils/typography'
 const Button = styled.button`
-  border-radius: 20px;
-  //height: 1.5em;
-  border: 1px solid lightgrey;
-  transition: all 0.3s;
-  
   padding: ${rhythm(2 / 16)} ${rhythm(12 / 16)};
+  border-radius: 20px;
+  text-transform: uppercase;
+  font-size: 1em;
+  transition: all 0.3s;
+  position: relative;
+  overflow: hidden;
+  z-index: 1;
   cursor: pointer;
   outline: none;
-  font-size: 1em;
-  color: #969696;
   background-color: transparent;
-  
-  &:hover {
-    border-color: #969696;
-    
-    color: #333;
+  border: 1px solid lightgrey;
+  color: #969696;
+
+  &:after {
+    content: '';
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: #fff;
+    border-radius: 20px;
+    z-index: -2;
+  }
+
+  &:before {
+    content: '';
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 0%;
+    height: 100%;
+    background-color: #008fb3;
+    transition: all 0.3s;
+    border-radius: 20px;
+    z-index: -1;
+  }
+  &:hover,
+  &:focus {
+    border-color: transparent;
+    color: #fff;
+    &:before {
+      width: 100%;
+    }
   }
 `
+
 export default class extends Component {
   render() {
     const { children, ...rest } = this.props
