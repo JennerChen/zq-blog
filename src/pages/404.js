f65bca023b39e8c47f6a4e688f590ce42564c770
zhangqing <zhangqing332@live.com>
2020-10-31 13:04:30 +0800

    chore:

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,7 +6,6 @@ class NotFoundPage extends React.Component {
     return (
       <Layout location={this.props.location}>
         <h1>Not Found (O_O)?</h1>
-        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
       </Layout>
     )
   }
