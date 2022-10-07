5af9ad8165b3c594465a7c1a186ff4c7e7437262
Qing  Zhang <zhangqing332@live.com>
2022-10-07 13:08:17 +0800

    feat: 升级google gtag

--- gatsby-config.js
+++ gatsby-config.js
@@ -95,27 +95,16 @@ module.exports = {
       options: {
         // You can add multiple tracking ids and a pageview event will be fired for all of them.
         trackingIds: [
-          "UA-131289526-1", // Google Analytics / GA
-//          "AW-CONVERSION_ID", // Google Ads / Adwords / AW
-//          "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+          "UA-131289526-1"
         ],
         // This object gets passed directly to the gtag config command
         // This config will be shared across all trackingIds
         gtagConfig: {
-//          optimize_id: "OPT_CONTAINER_ID",
-//          anonymize_ip: true,
-//          cookie_expires: 0,
         },
         // This object is used for configuration specific to this plugin
         pluginConfig: {
           // Puts tracking script in the head instead of the body
-          head: false,
-          // Setting this parameter is also optional
-//          respectDNT: true,
-          // Avoids sending pageview hits from custom paths
-//          exclude: ["/preview/**", "/do-not-track/me/too/"],
-          // Defaults to https://www.googletagmanager.com
-//          origin: "YOUR_SELF_HOSTED_ORIGIN",
+          head: false
         },
       },
     },
